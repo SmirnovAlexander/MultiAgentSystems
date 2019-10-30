f8947cc12d36e259ab02cfa2dbca872bfb691a8e
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2019-10-30 13:18:12 +0300

    cleaned up

--- AgentsMeanValue/src/main/java/ru/spbu/mas/App.java
+++
@@ -1,9 +0,0 @@
-package ru.spbu.mas;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        MainController mc = new MainController();
-        mc.initAgents();
-    }
-}
